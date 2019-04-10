964b411f2e37e38859eb3fb7d69bc06a04501b91
sungshon <49482512+sungshon@users.noreply.github.com>
2019-04-10 19:07:32 +0300

    Updater code

---
+++ upd/build.js
@@ -0,0 +1,18 @@
+
+var isWin = process.platform === 'win32'
+
+const fs = require('fs')
+const path = require('path')
+
+const pmsDir = path.join(__dirname, '..', 'PimpMyStremio')
+
+if (!fs.existsSync(pmsDir))
+	fs.mkdirSync(pmsDir)
+
+function ext() {
+	return (isWin ? '.exe' : '')
+}
+
+const { exec } = require('pkg')
+
+exec(['package.json', '--target', 'host', '--output', '../PimpMyStremio' + ext()])
---
+++ upd/index.js
@@ -0,0 +1,212 @@
+
+const github = 'sungshon/PimpMyStremio-updates'
+
+var isWin = process.platform === 'win32'
+
+function ext() { return (isWin ? '.exe' : '') }
+
+const os = require('os')
+const needle = require('needle')
+const path = require('path')
+const fs = require('fs')
+const rimraf = require('rimraf')
+const spawn = require('child_process').spawn
+
+function getBinDir() {
+	const rootDir = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share')
+
+	const configDir = path.join(rootDir, 'PimpMyStremio')
+
+	if (!fs.existsSync(configDir))
+		fs.mkdirSync(configDir)
+
+	const binDir = path.join(configDir, 'assets')
+
+	return binDir
+}
+
+function getVersion(binDir) {
+	const versionFile = path.join(binDir, '..', 'version')
+	return !fs.existsSync(versionFile) ? false : fs.readFileSync(versionFile)
+}
+
+function saveVersion(binDir, version) {
+	const versionFile = path.join(binDir, '..', 'version')
+	fs.writeFileSync(versionFile, version)
+	return true
+}
+
+function installed(binDir) {
+
+	const requiredBins = ['engine' + ext(), 'phantom', 'web', 'youtube-dl']
+
+	let isOk = true
+
+	requiredBins.some(file => {
+		if (!fs.existsSync(path.join(binDir, file))) {
+			console.log('Cannot start PimpMyStremio')
+			console.error('Missing required library: ' + file)
+			isOk = false
+			return true
+		}
+	})
+
+	return isOk
+
+}
+
+function zipBall() {
+	return new Promise((resolve, reject) => {
+		const files = new Array()
+		files.push('assets-' + process.platform + '-' + process.arch + '.zip')
+		files.push('assets-' + process.platform + '.zip')
+		needle.get('https://api.github.com/repos/' + github + '/releases', (err, resp, body) => {
+			if (body && Array.isArray(body) && body.length) {
+				const tag = body[0].tag_name
+				let zipBall
+				(body[0].assets || []).some(el => {
+					if (files.indexOf(el.name) > -1) {
+						zipBall = el.browser_download_url
+						return true
+					}
+				})
+				if (!zipBall) {
+					reject('Could not find a valid package for your Operating System: ' + process.platform)
+					return
+				}
+				resolve({ tag, zipBall })
+			} else {
+				if (err)
+					console.error(err)
+				reject('Could not get releases for: ' + github)
+			}
+		})
+	})
+}
+
+function installEngine(binDir, githubData) {
+	return new Promise((resolve, reject) => {
+		const dest = binDir
+
+		function installAddon() {
+			console.log('PimpMyStremio - Downloading new version')
+			needle('get', githubData.zipBall, { follow_max: 5 })
+			.then(zipFile => {
+				const tmpFile = path.join(os.tmpdir(), 'PimpMyStremio.zip')
+
+				if (fs.existsSync(tmpFile))
+					fs.unlinkSync(tmpFile)
+
+				fs.writeFileSync(tmpFile, zipFile.body)
+
+				console.log('PimpMyStremio - Finished downloading new version')
+
+				var extract = require('extract-zip')
+
+				console.log('PimpMyStremio - Unpacking new version')
+				extract(tmpFile, { dir: path.join(binDir, '..') }, function (err) {
+					fs.unlinkSync(tmpFile)
+					if (err) {
+						console.log('Unzip error:')
+						console.error(err)
+						reject()
+						return
+					} else {
+						console.log('PimpMyStremio - Finished unpacking new version')
+						saveVersion(binDir, githubData.tag)
+					}
+					console.log('Updated PimpMyStremio to ' + githubData.tag)
+					resolve()
+				})
+			})
+			.catch(err => {
+				console.log('Unzip error:')
+				console.error(err)
+				reject()
+			})
+		}
+
+		function cleanDir(dest, cb) {
+			fs.stat(dest, err => {
+				if (!err) {
+					console.log('PimpMyStremio - Removing old version')
+					rimraf(dest, () => {
+						cb()
+					})
+				} else if (err.code === 'ENOENT')
+					cb()
+			})
+		}
+
+		cleanDir(dest, installAddon)
+	})
+}
+
+function startEngine(binDir) {
+	const newEnv = JSON.parse(JSON.stringify(process.env))
+
+	newEnv['PMS_UPDATE'] = process.execPath
+
+	const atStartup = (process.argv || []).some(el => !!(el == '--startup'))
+
+	if (atStartup)
+		newEnv['PMS_STARTUP'] = '1'
+
+	const addonProc = spawn(path.join(binDir, 'engine' + ext()), [],
+		{
+			cwd: binDir,
+			env: newEnv
+		})
+
+	addonProc.stdout.on('data', data => {
+		if (data) {
+			console.log(data.toString().trim())
+//			var sData = String(data).replace(/(\r\n\t|\n|\r\t)/gm,'')
+		}
+
+	})
+
+	addonProc.stderr.on('data', data => {
+		if (data) {
+			console.log(data.toString().trim())
+//			var sData = String(data).replace(/(\r\n\t|\n|\r\t)/gm,'')
+		}
+	})
+
+	addonProc.on('exit', code => {
+		console.log('Process exit, code: ' + code)
+		process.exit()
+	})
+
+}
+
+const binDir = getBinDir()
+
+function afterInstall() {
+	const isInstalled = installed(binDir)
+	if (isInstalled)
+		startEngine(binDir)
+	else {
+		console.error('Could not start PimpMyStremio, binaries missing')
+		process.exit()
+	}
+}
+
+console.log('PimpMyStremio - Checking for updates')
+
+zipBall().then(githubData => {
+
+	const version = getVersion(binDir)
+
+	if (!version || version != githubData.tag) {
+		console.log('PimpMyStremio - New version found')
+		installEngine(binDir, githubData).then(afterInstall).catch(afterInstall)
+	} else {
+		console.log('PimpMyStremio - Already running latest version')
+		startEngine(binDir)
+	}
+
+}).catch(err => {
+	console.error(err)
+	afterInstall()
+})
