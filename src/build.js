f1622ae5f733268be0f3be627a7c43ec4cf815d7
sungshon <49482512+sungshon@users.noreply.github.com>
2019-04-10 19:08:26 +0300

    Initial code

---
+++ src/build.js
@@ -0,0 +1,85 @@
+
+var isWin = process.platform === 'win32'
+
+const fs = require('fs')
+const path = require('path')
+const ncp = require('ncp').ncp
+
+const assetsDir = path.join(__dirname, '..', 'assets')
+
+if (!fs.existsSync(assetsDir))
+	fs.mkdirSync(assetsDir)
+
+const modulesDir = path.join(__dirname, 'node_modules')
+
+function ext() {
+	return (isWin ? '.exe' : '')
+}
+
+const spawn = require('child_process').spawn
+
+function npm(dest, cb) {
+	spawn('npm', ['i', '--production'], {
+		cwd: dest,
+		env: JSON.parse(JSON.stringify(process.env))
+	}).on('exit', cb)
+}
+
+function copyWeb(cb) {
+	console.log('Start - Copy Web Content')
+	ncp('./web', '../assets/web', function (err) {
+		if (err)
+			return console.error(err)
+		console.log('End - Copy Web Content')
+		cb()
+	})
+}
+
+function copyYoutube(cb) {
+	console.log('Start - Copy Youtube-dl')
+	ncp('./node_modules/youtube-dl', '../assets/youtube-dl', function (err) {
+		if (err)
+			return console.error(err)
+		console.log('End - Copy Youtube-dl')
+		console.log('Start - Build Youtube-dl')
+		npm(path.join(__dirname, '..', 'assets', 'youtube-dl'), () => {
+			console.log('End - Build Youtube-dl')
+			cb()
+		})
+	})
+}
+
+function copyPhantom(cb) {
+	console.log('Start - Copy PhantomJS')
+	ncp('./node_modules/phantom', '../assets/phantom', function (err) {
+		if (err)
+			return console.error(err)
+		console.log('End - Copy PhantomJS')
+		console.log('Start - Build PhantomJS')
+		npm(path.join(__dirname, '..', 'assets', 'phantom'), () => {
+			console.log('End - Build PhantomJS')
+			cb()
+		})
+	})
+}
+
+function packageApp() {
+
+	console.log('Start - Packaging App to Executable')
+
+	const { exec } = require('pkg')
+
+	exec(['package.json', '--target', 'host', '--output', '../assets/engine' + ext()]).then(() => {
+		console.log('Finished!')
+	}).catch(err => {
+		console.log('Finished!')
+	})
+}
+
+copyWeb(() => {
+	copyYoutube(() => {
+		copyPhantom(() => {
+			packageApp()
+		})
+	})
+})
---
+++ src/index.js
@@ -0,0 +1,131 @@
+
+const pkg = require('./lib/pkg')
+const express = require('express')
+const getPort = require('get-port')
+const opn = require('open')
+const tunnel = require('./lib/tunnels/serveo')
+const autoLaunch = require('./lib/autoLaunch')
+const addon = require('./lib/addon')
+const uConfig = require('./lib/config/userConfig')
+const userConfig = uConfig.read().userDefined
+const runningAddons = uConfig.read().runningAddons
+const proxy = require('./lib/proxy')
+const persist = require('./lib/persist')
+const cleanUp = require('./lib/cleanUp')
+const sideload = require('./lib/sideload')
+const login = require('./lib/login')
+const querystring = require('querystring')
+const path = require('path')
+const confDir = require('./lib/dirs/configDir')
+
+const isStartup = process.env['PMS_STARTUP']
+
+autoLaunch('PimpMyStremio', userConfig.autoLaunch)
+
+let serverPort
+
+async function runServer() {
+	const router = express()
+
+	const respond = (res, data) => {
+	  if ((data || {}).cacheMaxAge)
+	  	res.setHeader('Cache-Control', 'max-age=' + data.cacheMaxAge)
+	  res.setHeader('Access-Control-Allow-Origin', '*')
+	  res.setHeader('Access-Control-Allow-Headers', '*')
+	  res.setHeader('Content-Type', 'application/json')
+	  res.send(data)
+	}
+
+	const fail = res => {
+		res.writeHead(500)
+		res.end(JSON.stringify({ err: 'handler error' }))
+	}
+
+	router.use(express.static(process.env['PMS_UPDATE'] ? path.join(confDir(), 'assets', 'web') : 'web'))
+
+	router.get('/login-api', (req, res) => {
+		const query = req.query || {}
+		const method = query.method
+		const val = query.val
+		if (login[method]) {
+			login[method](val).then(resp => {
+				respond(res, resp)
+			}).catch(err => {
+				console.error(err)
+				fail(res)
+			})
+		} else
+			fail(res)
+	})
+
+	router.get('/api', (req, res) => {
+		const query = req.query || {}
+		const method = query.method
+		const name = query.name
+		const pass = uConfig.readPass()
+		if (pass && query.pass != pass) {
+			fail(res)
+			return
+		}
+		if (addon[method] && method != 'init') {
+			addon[method](addon.getManifest(name), decodeURIComponent(query.payload)).then(resp => {
+				respond(res, resp)
+			}).catch(err => {
+				console.error(err)
+				fail(res)
+			})
+		} else
+			fail(res)
+	})
+
+	router.get('/catalog.json', (req, res) => {
+		const reqHost = req.headers.host
+		const host = reqHost ? req.protocol + (reqHost.includes('.serveo.net') ? 's' : '') + '://' + reqHost : false
+		addon.getCatalog(host, serverPort).then(resp => {
+			respond(res, resp)
+		}).catch(err => {
+			console.error(err)
+			fail(res)
+		})
+	})
+
+	function getRouter(req, res) {
+		const vmAddon = addon.get(req.params.addonName)
+		if ((vmAddon || {}).router)
+			vmAddon.router(req, res, () => {
+				res.statusCode = 404
+				res.end()
+			})
+		else
+			fail(res)
+	}
+
+	router.get('/:addonName/manifest.json', getRouter)
+
+	router.get('/:addonName/:resource/:type/:id/:extra?.json', getRouter)
+
+	serverPort = await getPort({ port: userConfig.serverPort })
+
+	proxy.createProxyServer(router)
+
+	const server = router.listen(serverPort, () => {
+
+		cleanUp.set(server)
+
+		const url = 'http://127.0.0.1:' + serverPort
+
+		proxy.setEndpoint(url)
+
+		console.log('PimpMyStremio server running at: ' + url)
+
+        if (userConfig.remote)
+            tunnel(serverPort, { subdomain: userConfig.subdomain }) 
+        else if (!isStartup)
+        	opn('http://127.0.0.1:' + serverPort)
+
+	})
+}
+
+addon.init(runningAddons, () => {
+	sideload.loadAll(runServer)
+}, cleanUp.restart)
