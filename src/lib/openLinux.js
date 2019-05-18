2459b5f747cef4d17629876a6e44134a3f1c92d3
sungshon <49482512+sungshon@users.noreply.github.com>
2019-05-18 11:14:40 +0300

    Delete openLinux.js

--- src/lib/openLinux.js
+++
@@ -1,9 +0,0 @@
-
-const path = require('path')
-const configDir = require('./dirs/configDir')()
-
-module.exports = url => {
-	try {
-		require('child_process').spawn(path.join(configDir, 'assets', 'xdg-open'), [ url ], { stdio: 'ignore', detached: true })
-	} catch(e) {}
-}
