049d9d885838325004426042e5ed32419c2f7fa9
sungshon <49482512+sungshon@users.noreply.github.com>
2019-05-21 18:12:18 +0300

    Add subtitle converter libs to pkg dummies

--- src/lib/pkg.js
+++ src/lib/pkg.js
@@ -5,6 +5,15 @@ function dummies() {
 	require('vm2/lib/sandbox')
 	require('vm2/lib/contextify')
 	require('vm2/lib/wildcard')
+	require('subsrt/lib/format/ass')
+	require('subsrt/lib/format/json')
+	require('subsrt/lib/format/lrc')
+	require('subsrt/lib/format/sbv')
+	require('subsrt/lib/format/smi')
+	require('subsrt/lib/format/srt')
+	require('subsrt/lib/format/ssa')
+	require('subsrt/lib/format/sub')
+	require('subsrt/lib/format/vtt')
 }
 
 module.exports = () => {}
