8175073281b8937d00689f690e06328cbc160aca
sungshon <49482512+sungshon@users.noreply.github.com>
2019-06-12 21:11:59 +0300

    Change local data dir if it does not exists

--- src/lib/dirs/rootDir.js
+++ src/lib/dirs/rootDir.js
@@ -1,1 +1,12 @@
-module.exports = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share')
+const fs = require('fs')
+const path = require('path')
+
+let rootDir = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + '/.local/share')
+
+if (!fs.existsSync(rootDir)) {
+	rootDir = process.env['PMS_PACKAGED'] ? path.dirname(process.execPath) : path.join(__dirname, '..', '..', '..')
+	process.env['PMS_CONFIG_DIR_NAME'] = 'LocalData'
+}
+
+
+module.exports = rootDir
