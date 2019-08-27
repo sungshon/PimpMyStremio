b8d96055eaaf916cc7c374af93b873df98e1a2a9
sungshon <49482512+sungshon@users.noreply.github.com>
2019-08-27 18:21:41 +0300

    Add option to inject polyfill in phantom-js

--- src/lib/phantom.js
+++ src/lib/phantom.js
@@ -31,6 +31,8 @@ module.exports = {
 			opts2 = { logLevel: 'warn' }; warn('Second options are not an object')
 		}
 
+		let onceCb
+
 		phantom.create(clArgs, opts2)
 		.then(function(phInstance) {
 			instance = phInstance
@@ -61,12 +63,22 @@ module.exports = {
 //				console.log('Page Timed Out')
 //			})
 
-			cb(instance, page)
+			function end() {
+				onceCb = true
+				cb(instance, page)
+			}
+
+			if (!opts.polyfill)
+				end()
+			else {
+				injectCoreJs(instance, page, end)
+			}
 
 		}).catch(err => {
 	      console.log('PhantomJS - Caught Error')
 	      console.error(err)
-	      cb()
+	      if (!onceCb)
+		      cb()
 	    })
 	},
 	close: (instance = {}, page = {}, cb = (() => {})) => {
@@ -91,4 +103,79 @@ module.exports = {
 		})
 
 	}
+}
+
+function injectCoreJs(instance, page, cb) {
+	// we'll hack our way to glory here
+	// by injecting core.js into phantomjs
+	// to support ES 5/6
+
+	const myPage = {}
+	let pageChanged
+
+	for (let key in page) {
+		if (typeof page[key] === 'function') {
+			if (key == 'open')
+				myPage[key] = (a,b,c,d) => {
+					return new Promise((resolve, reject) => {
+
+						page.on('onLoadFinished', async status => {
+							if (pageChanged) {
+								const body = await page.property('content')
+								resolve(status, body)
+							}
+						})
+
+						let once
+
+						page.on('onInitialized', function() {
+						  if (once) return
+
+						  once = true
+
+						  page.evaluate(function() {
+
+						  	var script = document.createElement('script')
+							script.type = 'text/javascript'
+							script.src = 'https://unpkg.com/core-js-bundle/minified.js'
+							script.async = false
+
+							var parent = document.getElementsByTagName('head')[0]
+
+							parent.insertBefore(script, parent.firstChild)
+							return true
+						  }).then(async resp => {
+						  	const pgContent = await page.property('content')
+						  	const pgUrl = await page.property('url')
+						  	page.setContent(pgContent, pgUrl)
+						  	pageChanged = true
+						  }).catch(e => {
+						  	console.log('err')
+						  	reject(e)
+						  })
+						})
+
+						page.open(a,b,c,d).then(() => {
+							// ignore this.. it's useless in this case
+						}).catch(e => {
+							reject(e)
+						})
+					})
+				}
+			else
+				myPage[key] = (a,b,c,d) => { return page[key](a,b,c,d) }
+		}
+	}
+
+	myPage['on'] = (a,b) => {
+		page.on(a, (c,d,e,f) => { if (pageChanged) b(c,d,e,f) })
+	}
+
+	const extras = ['property']
+
+	extras.forEach(key => {
+		myPage[key] = (a,b) => { return page[key](a,b) }
+	})
+
+	cb()
 }
