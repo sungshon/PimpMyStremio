eacd14d4505c10a1ed94c1567a5ee63eceb5d673
sungshon <49482512+sungshon@users.noreply.github.com>
2019-09-12 14:52:34 +0300

    Support custom sideload directory

--- src/lib/dirs/sideloadDir.js
+++ src/lib/dirs/sideloadDir.js
@@ -9,10 +9,20 @@ module.exports = () => {
 	if (sideloadDir)
 		return sideloadDir
 
-	sideloadDir = path.join(configDir, 'sideload')
+	if (process.env['PMS_SIDELOAD']) {
+		const dir = process.env['PMS_SIDELOAD'].split('"').join('')
+		if (fs.existsSync(dir))
+			sideloadDir = dir
+		else
+			console.log('Warning: Custom sideload directory leads to non-existent folder, using default path.')
+	}
+	
+	if (!sideloadDir) {
+		sideloadDir = path.join(configDir, 'sideload')
 
-	if (!fs.existsSync(sideloadDir))
-		fs.mkdirSync(sideloadDir)
+		if (!fs.existsSync(sideloadDir))
+			fs.mkdirSync(sideloadDir)
+	}
 
 	return sideloadDir
 }
