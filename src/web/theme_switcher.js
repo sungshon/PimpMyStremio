c70ccbd93d5f7953ba9365573a44cd0c57269529
sungshon <49482512+sungshon@users.noreply.github.com>
2020-01-15 15:31:56 +0200

    Themes now use current user config logic
    It removes the "toggle light" effect and requires restart to save the theme setting, but i think it's better for all settings to work the same..

--- src/web/theme_switcher.js
+++ src/web/theme_switcher.js
@@ -1,23 +1,14 @@
 const availableThemes = ['default', 'dark'];
-let theme = localStorage.getItem('theme') || 'default';
 
 function insertTheme() {
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.id = 'theme';
-    link.href = '/themes/' + theme + '.css';
-    document.head.appendChild(link);
-}
-
-function toggleTheme() {
-    // get next theme
-    let nextIndex = availableThemes.indexOf(theme) + 1;
-    if (nextIndex > availableThemes.length - 1)
-        nextIndex = 0;
-
-    theme = availableThemes[nextIndex];
-
-    // apply theme
-    $('#theme').attr('href', '/themes/' + theme +  '.css');
-    localStorage.setItem('theme', theme);
+    request('addonConfig', '_pimpmystremio', '', userConfig => {
+        let themeOpt = 0;
+        if (userConfig.darkMode) themeOpt = 1;
+        const theme = availableThemes[themeOpt];
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.id = 'theme';
+        link.href = '/themes/' + theme + '.css';
+        document.head.appendChild(link);
+    })
 }
