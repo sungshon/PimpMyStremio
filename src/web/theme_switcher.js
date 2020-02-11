4b3f3695ab736de4d8343702958a462e51d9ef9a
sungshon <49482512+sungshon@users.noreply.github.com>
2020-02-11 14:02:26 +0200

    Extended theming support

--- src/web/theme_switcher.js
+++ src/web/theme_switcher.js
@@ -1,14 +1,77 @@
-const availableThemes = ['default', 'dark'];
+
+const availableThemes = {
+    'Light': {
+        material_ui_colors: ['orange', 'indigo'],
+        styles: 'default.css',
+    },
+    'Dark': {
+        material_ui_colors: ['orange', 'indigo'],
+        styles: 'dark.css',
+    },
+    'Rusty': {
+        material_ui_colors: ['grey', 'orange'],
+        styles: 'rust.css',
+    },
+    'Sky': {
+        material_ui_colors: ['brown', 'blue'],
+        styles: 'sky.css',
+    },
+    'Light Orange': {
+        material_ui_colors: ['amber', 'orange'],
+        styles: 'default.css',
+    },
+    'Light Blue': {
+        material_ui_colors: ['indigo', 'blue'],
+        styles: 'default.css',
+    },
+    'Light Green': {
+        material_ui_colors: ['light_green', 'green'],
+        styles: 'default.css',
+    },
+    'Dark Purple': {
+        material_ui_colors: ['deep_orange', 'deep_purple'],
+        styles: 'dark.css',
+    },
+    'Dark Blue': {
+        material_ui_colors: ['blue', 'indigo'],
+        styles: 'dark.css',
+    },
+    'Dark Green': {
+        material_ui_colors: ['teal', 'green'],
+        styles: 'dark.css',
+    },
+};
+
+function stylesLoaded(cssStylesheet, cb) {
+    // this is a hack, but the only cross-browser solution I could find
+    var img = document.createElement('img')
+    img.onerror = cb
+    img.src = cssStylesheet
+}
+
+function injectCss(cssFile, cb) {
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.href = cssFile
+    link.type = 'text/css'
+    if (cb)
+        stylesLoaded(link, cb)
+    document.head.appendChild(link)
+}
 
 function insertTheme() {
     request('addonConfig', '_pimpmystremio', '', userConfig => {
-        let themeOpt = 0;
-        if (userConfig.darkMode) themeOpt = 1;
-        const theme = availableThemes[themeOpt];
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.id = 'theme';
-        link.href = '/themes/' + theme + '.css';
-        document.head.appendChild(link);
+
+        const theme = availableThemes[userConfig.theme] || availableThemes['Light']
+
+        if (!theme.material_ui_colors || !validateColors(theme.material_ui_colors))
+            theme.material_ui_colors = availableThemes['Light'].material_ui_colors
+
+        if (!theme.styles)
+            theme.styles = availableThemes['Light'].styles
+
+        // load material ui colors first
+        injectCss(getColorsCss(theme.material_ui_colors), injectCss.bind(null, '/themes/' + theme.styles))
+
     })
 }
